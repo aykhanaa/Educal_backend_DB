6e758b356fba0966fac4d652271103813286193f
unknown <aykhanaa@code.edu.az>
2024-06-03 08:13:32 +0400

    create educal back

---
+++ Educal_backend/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Educal_backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Composition;
+using System.Reflection.Metadata;
+
+namespace Educal_backend.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseImage> CourseImages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasQueryFilter(m => !m.SoftDeleted);
+            modelBuilder.Entity<Course>().HasQueryFilter(m => !m.SoftDeleted);
+        }
+    }
+}
---
+++ Educal_backend/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace Educal_backend.Models
+{
+    public class Course:BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<CourseImage> CourseImages { get; set; }
+    }
+}
